6c4e690d7b6be496f4c8f509a2eae3a8e70b9c2a
holtc <holt.chris19@gmail.com>
2014-12-09 16:20:15 -0500

    all files in correct places, tweets.csv is now in data/raw, currently almost 17k tweets for apple and google

---
+++ src/tscraper.py
@@ -0,0 +1,84 @@
+""" NETS 213 Final Project
+    Twitter Scraper
+
+"""
+import base64
+import json
+
+from Database import *
+from Tweet import *
+from TwitterSearch import *
+
+def get_access_token():
+    ''' Implement application-only authentication, as described here:
+    https://dev.twitter.com/docs/auth/application-only-auth
+
+    Note: to base64 encode a string in Python, use the b64encode
+    method of the base64 class. See here:
+    https://docs.python.org/2/library/base64.html
+
+    This function should return an access token, which can then be used
+    to make authenticated requests in the remainder of the homework.
+
+    With regards to style, it's worth thinking about how many times
+    you should need to call this function.
+    '''
+    ckey = 'U0P64ublAENwHcZ5lwgCADLHH'
+    csecret = 'n19TRpI1rXhZrXtgmFvY61csTJS1g4mk2n6aL2HnTtGTfQeCVf'
+    creds = base64.b64encode('{}:{}'.format(ckey, csecret))
+    ct = 'application/x-www-form-urlencoded;charset=UTF-8'
+    headers = {'Authorization': 'Basic {}'.format(creds), 'Content-Type': ct}
+    data = 'grant_type=client_credentials'
+    url = 'https://api.twitter.com/oauth2/token'
+    req = requests.post(url, headers=headers, data=data)
+    acc_tok = req.json()['access_token']
+    return 'Bearer {}'.format(acc_tok)
+        
+def get_since_id(db, search_query):
+        max_id = 0
+        query = 'call get_max_ids()'
+        result = db.execute(query)
+        for r in result:
+            if r[1] == search_query:
+                return long(r[0])
+        return max_id
+
+    
+def search_twitter(atoken, query, db):
+    count = 1
+    since_id = get_since_id(db, query)
+    ts = TwitterSearch(atoken, query, since_id, None, '2014-12-11')
+    #make sure there are tweets returned
+    num_tweets = 0
+    while ts.tweets and count < 400:
+        count += 1
+        if (count % 25 == 0):
+            print str(count) + " requests for: " + query + "....."
+        #save it here because we know there are tweets to save
+        #now get next batch of tweets
+        #params are query, since_id and max_id, eg the range of tweets to search
+        ts = TwitterSearch(atoken, query, since_id, ts.min_id - 1, None)
+        num_tweets += ts.save_results(db)
+    print str(count) + " total requests for: " + query 
+    print str(num_tweets) + " tweets saved for: " + query + '\n'
+    return count
+
+def main():
+    import doctest
+    atoken = get_access_token()
+    host = 'localhost'
+    user = 'nets'
+    pwd = 'test'
+    db_name = 'nets'
+    db = Database(host, user, pwd, db_name)
+    options = (doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS)
+    doctest.testmod(optionflags=options)
+    queries = ["#AAPL OR @Apple", "#GOOG OR @Google"]
+    total_requests = 0
+    for query in queries:
+        total_requests += search_twitter(atoken, query, db)
+    print str(total_requests) + " requests made to Twitter API"
+
+
+if __name__ == "__main__":
+    main()
--- src/tweets_to_csv.py
+++ src/tweets_to_csv.py
@@ -2,11 +2,10 @@ from Tweet import *
 from Database import *
 
 def print_all_tweets_to_csv(db):
-    f = open("tweets.csv", 'w')
+    f = open("../data/raw/tweets.csv", 'w')
     query = "select * from nets.tweet"
     result = db.execute(query)
     f.write("id,date,text,query\n")
-    print result[1]
     for r in result:
         text = r[2]
         s = ''.join([str(r[0]), ",", r[1], ',"', text, '",', r[3], "\n"])
