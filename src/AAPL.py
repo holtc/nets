177afb7e96eeea7d2a8339b5f5a829775eec9e6f
holtc <holt.chris19@gmail.com>
2014-12-14 23:23:58 -0500

    made changes

--- src/AAPL.py
+++ src/AAPL.py
@@ -11,46 +11,29 @@ def smooth(close_list, beta):
         smooth.append(smoothed)
     return smooth
 
-with open('AAPL_formatted.csv','r') as f:
+with open('../Data/raw/AAPL_formatted.csv','r') as f:
     reader = csv.DictReader(f, delimiter=',')
     data = list(reader)
-    
-position_by_hour = {}
+
+f.close()
+
+close_by_day_hour = {}
 
 for row in data:
     day = row['timestamp'][0:10]
     hour = row['timestamp'][11:13]
-    minute = row['timestamp'][14:15]
-    if day not in position_by_hour.keys():
-        position_by_hour[day] = {}
-    if hour not in position_by_hour[day].keys():
-        position_by_hour[day][hour] = {}
-    if minute not in position_by_hour[day][hour].keys():
-        position_by_hour[day][hour][minute] = []
-    position_by_hour[day][hour][minute].append(row)
-# print position_by_hour['2014-12-01']['14']
+    if day + " " + hour not in close_by_day_hour.keys():
+        close_by_day_hour[day + " " + hour] = []
+    close_by_day_hour[day + " " + hour].append(float(row['close']))
+    
+buckets_dict = {}
 
-avg_pos_dict = {}
+for k in close_by_day_hour.keys():
+    buckets_dict[k] = sum(close_by_day_hour[k]) / float(len(close_by_day_hour[k]))
 
-for day in position_by_hour.keys():
-    for hour in position_by_hour[day].keys():
-        for minute in position_by_hour[day][hour].keys():
-            position = 0
-            count = 0
-            for pos in position_by_hour[day][hour][minute]:
-                count += 1;
-                position += float(pos['open'])
-            avg_pos_dict[day + " " + hour + ":" + minute + "0:00"] = position/float(count)
-
-datetime_pos_dict = {}
-for e in avg_pos_dict:
-    dt = datetime.datetime.strptime(e, '%Y-%m-%d %H:%M:%S')
-    dt = dt.replace(hour=dt.hour - 5)
-    datetime_pos_dict[dt] = avg_pos_dict[e]
-
-x = datetime_pos_dict.keys()
-print x[0].hour
-y = datetime_pos_dict.values()
+sorted_keys = sorted(buckets_dict)
+x = [datetime.datetime.strptime(e, '%Y-%m-%d %H') for e in sorted_keys]
+y = smooth([buckets_dict[i] for i in sorted_keys], .6)
 
 # plt.scatter(x,y)
 # plt.show()
@@ -61,15 +44,15 @@ y = datetime_pos_dict.values()
 # for i in indexes_to_sort_by:
 #     x_ordered.append(x[i])
 #     y_ordered.append(x[i]) 
-y_sorted = []
+##y_sorted = []
+##
+##sorted_x = sorted(x)
+##for i in sorted_x:
+##    y_sorted.append(y[i])
+##
+##y_sorted_smooth = smooth(y_sorted, 0.6)
 
-sorted_pos_dict = sorted(datetime_pos_dict)
-for i in sorted_pos_dict:
-    y_sorted.append(datetime_pos_dict[i])
-
-y_sorted_smooth = smooth(y_sorted, 0.6)
-
-plt.plot(sorted_pos_dict,y_sorted_smooth, marker='o')
+plt.plot(x,y, marker='o')
 plt.xlim(datetime.datetime(2014, 12, 1, 0, 0),datetime.datetime(2014, 12, 11, 2, 0))
 plt.show()
 
--- src/Sentiment_analysis.py
+++ src/Sentiment_analysis.py
@@ -13,85 +13,148 @@ def smooth(close_list, beta):
     return smooth
 
 
-with open('aggregated.csv','r') as f:
+with open('../Data/aggregated.csv','r') as f:
     reader = csv.DictReader(f, delimiter=',')
     data = list(reader)
 
-    
+f.close()
 
-tweets_by_hour = {}
+tweets_by_day_hour = {}
 
 for row in data:
-    day = row['date'][0:10]
+    day = row['date'][8:10]
     hour = row['date'][11:13]
-    minute = row['date'][14:16]
-    if (int(hour) < 9 or int(hour) > 16) : continue
-    if (int(hour) >= 9 and int(hour) < 10 and int(minute) < 30) : continue
-    if day not in tweets_by_hour.keys():
-        tweets_by_hour[day] = {}
-    if hour not in tweets_by_hour[day].keys():
-        tweets_by_hour[day][hour] = {}
-    minute = minute[0:1]
-    if minute not in tweets_by_hour[day][hour].keys():
-        tweets_by_hour[day][hour][minute] = []
+    minute = row['date'][14:15]
+    if int(hour) < 14 and int(minute) < 30:
+        continue
+    if int(hour) > 21:
+        continue
+    if int(day) < 2:
+        continue
+    if int(day) == 6 or int(day) == 7:
+        continue
+    day = ''.join(['2014-12-', day])
+    if day + " " + hour not in tweets_by_day_hour.keys():
+        tweets_by_day_hour[day + " " + hour] = []
+    if not (row['sentiment'] == 'not_relevant'):
+        tweets_by_day_hour[day + " " + hour].append((int(row['sentiment']) - 3)/2)
+
+buckets_dict = {}
+
+for k in tweets_by_day_hour.keys():
+    buckets_dict[k] = sum(tweets_by_day_hour[k]) / float(len(tweets_by_day_hour[k]))
+
+sorted_keys = sorted(buckets_dict)
+x1 = [datetime.datetime.strptime(e, '%Y-%m-%d %H') for e in sorted_keys]
+y1 = smooth([buckets_dict[i] for i in sorted_keys], .6)
+plt.figure(1)
+plt.plot(x1,y1, marker='o')
+plt.xlim(datetime.datetime(2014, 12, 1, 0, 0),datetime.datetime(2014, 12, 11, 2, 0))
+#plt.show()
+
+with open('../Data/raw/AAPL_formatted.csv','r') as f:
+    reader = csv.DictReader(f, delimiter=',')
+    data = list(reader)
+
+f.close()
+
+close_by_day_hour = {}
+
+for row in data:
+    day = row['timestamp'][0:10]
+    hour = row['timestamp'][11:13]
+    if int(day[8:10]) < 2 or int(day[8:10]) > 9:
+        continue
+    if day + " " + hour not in close_by_day_hour.keys():
+        close_by_day_hour[day + " " + hour] = []
+    close_by_day_hour[day + " " + hour].append(float(row['close']))
     
-    tweets_by_hour[day][hour][minute].append(row)
-    
-score_dict = {}
-average_score_dict = {}
+buckets_dict = {}
 
-for day in tweets_by_hour.keys():
-    for hour in tweets_by_hour[day].keys():
-        for minute in tweets_by_hour[day][hour].keys(): 
-            score = 0
-            tweet_num = 0
-            for tweet in tweets_by_hour[day][hour][minute]:
-                tweet_num += 1;
-                if tweet['sentiment'] == '5':
-                    score += 1
-                elif tweet['sentiment'] == '1':
-                    score -= 1
-            score_dict[day + " " + hour + ":" + minute + "0:00"] = score
-            average_score_dict[day + " " + hour + ":" + minute + "0:00"] = float(score)/float(tweet_num)
+for k in close_by_day_hour.keys():
+    buckets_dict[k] = sum(close_by_day_hour[k]) / float(len(close_by_day_hour[k]))
 
-# print score_dict['Tue Dec 02 05:00:00']
-
-datetime_score_dict = {}
-
-average_datetime_score_dict = {}
-for e in score_dict:
-    datetime_score_dict[datetime.datetime.strptime(e, '%a %b %d %H:%M:%S')] = score_dict[e]
-    average_datetime_score_dict[datetime.datetime.strptime(e, '%a %b %d %H:%M:%S')] = average_score_dict[e]
+sorted_keys = sorted(buckets_dict)
+x = [datetime.datetime.strptime(e, '%Y-%m-%d %H') for e in sorted_keys]
+y = smooth([buckets_dict[i] for i in sorted_keys], .6)
 
 
-sorted_scores = sorted(datetime_score_dict)
-avg_sorted_scores = sorted(average_datetime_score_dict)
+import numpy as np
+for i in range(6):
+    print np.corrcoef(y[i*8:8*(i+1)],y1[i*8:8*(i+1)])[1,0]
 
+# plt.scatter(x,y)
+# plt.show()
 
-y_sorted = []
-y_avg_sorted = []
-for i in avg_sorted_scores:
-    y_sorted.append(datetime_score_dict[i])
-    y_avg_sorted.append(average_datetime_score_dict[i])
+# indexes_to_sort_by = numpy.array(x)
+# x_ordered = [] 
+# y_ordered = [] 
+# for i in indexes_to_sort_by:
+#     x_ordered.append(x[i])
+#     y_ordered.append(x[i]) 
+##y_sorted = []
+##
+##sorted_x = sorted(x)
+##for i in sorted_x:
+##    y_sorted.append(y[i])
+##
+##y_sorted_smooth = smooth(y_sorted, 0.6)
+plt.figure(2)
+plt.plot(x,y, marker='o')
+plt.xlim(datetime.datetime(2014, 12, 1, 0, 0),datetime.datetime(2014, 12, 11, 2, 0))
 
-y_avg_sorted_smooth = smooth(y_avg_sorted, 0.7)
+##for day in tweets_by_hour.keys():
+##    for hour in tweets_by_hour[day].keys():
+##        for minute in tweets_by_hour[day][hour].keys(): 
+##            score = 0
+##            tweet_num = 0
+##            for tweet in tweets_by_hour[day][hour][minute]:
+##                tweet_num += 1;
+##                if tweet['sentiment'] == '5':
+##                    score += 1
+##                elif tweet['sentiment'] == '1':
+##                    score -= 1
+##            score_dict[day + " " + hour + ":" + minute + "0:00"] = score
+##            average_score_dict[day + " " + hour + ":" + minute + "0:00"] = float(score)/float(tweet_num)
+##
+### print score_dict['Tue Dec 02 05:00:00']
+##
+##datetime_score_dict = {}
+##
+##average_datetime_score_dict = {}
+##for e in score_dict:
+##    datetime_score_dict[datetime.datetime.strptime(e, '%a %b %d %H:%M:%S')] = score_dict[e]
+##    average_datetime_score_dict[datetime.datetime.strptime(e, '%a %b %d %H:%M:%S')] = average_score_dict[e]
+##
+##
+##sorted_scores = sorted(datetime_score_dict)
+##avg_sorted_scores = sorted(average_datetime_score_dict)
+##
+##
+##y_sorted = []
+##y_avg_sorted = []
+##for i in avg_sorted_scores:
+##    y_sorted.append(datetime_score_dict[i])
+##    y_avg_sorted.append(average_datetime_score_dict[i])
+##
+##y_avg_sorted_smooth = smooth(y_avg_sorted, 0.7)
+##
+##average = numpy.mean(y_avg_sorted)
+##stdev = numpy.std(y_avg_sorted)
+##zscores = []
+##for i in y_avg_sorted:
+##    zscores.append((i - average)/float(stdev))
+##
+##
+##average_smooth = numpy.mean(y_avg_sorted_smooth)
+##stdev_smooth = numpy.std(y_avg_sorted_smooth)
+##zscores_smooth = []
+##for i in y_avg_sorted_smooth:
+##    zscores_smooth.append((i - average_smooth)/float(stdev_smooth))
 
-average = numpy.mean(y_avg_sorted)
-stdev = numpy.std(y_avg_sorted)
-zscores = []
-for i in y_avg_sorted:
-    zscores.append((i - average)/float(stdev))
-
-
-average_smooth = numpy.mean(y_avg_sorted_smooth)
-stdev_smooth = numpy.std(y_avg_sorted_smooth)
-zscores_smooth = []
-for i in y_avg_sorted_smooth:
-    zscores_smooth.append((i - average_smooth)/float(stdev_smooth))
-
-plt.plot(sorted_scores, zscores_smooth, marker='o')
-plt.xlim(datetime.datetime(1900, 12, 1, 10, 0),datetime.datetime(1900, 12, 11, 2, 0))
-
+##plt.plot(sorted_scores, zscores_smooth, marker='o')
+##plt.xlim(datetime.datetime(1900, 12, 1, 10, 0),datetime.datetime(1900, 12, 11, 2, 0))
+##
 plt.show()
 
 
